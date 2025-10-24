1ab471190b267d6e7df482c3ba49f46caffc11b4
ady7ady7 <adrianoovv2@gmail.com>
2025-10-24 13:08:23 +0200

    I've added the mock plan for th estructure to main.py in /otodom-scaper, but honestly I have my doubts if it's even worth it to actually work on this - it's a short code, and doing it that way, and creating the structure  might even raise more complexity than the actual implementation. I'm not yet sure if we will get much better readability from it. WILL THINK ABOUT IT

---
+++ otodom-scraper/main.py
@@ -0,0 +1,11 @@
+
+
+#initiate_selenium
+#skip_the_cookie_button
+#fetch_relevant info from config (max pages) 
+#might probably also initiate a loop here
+##scroll_to_the_bottom
+#fetch_the_list_of_links_from_the_current_page
+#loop through all of the links from the list
+#switch_to_the_next_page
+#handle_errors
